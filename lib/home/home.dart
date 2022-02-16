66191db88073a7c7b34079fce0a25edae0d6f4e0
Arthur Nunes <art.silvarn@gmail.com>
2022-02-15 21:00:23 -0300

    feature:#1 - initialized repository

---
+++ lib/home/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: Container(),
+    );
+  }
+}
