f7734576ad945c2ad73681e210a5ef23c2453104
Arthur Nunes <art.silvarn@gmail.com>
2022-02-19 16:39:25 -0300

    added button component

---
+++ lib/components/elevated_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ElevatedButtonApp extends StatelessWidget {
+  const ElevatedButtonApp(
+      {Key? key, required this.callBack, required this.textButton})
+      : super(key: key);
+
+  final void Function()? callBack;
+  final String textButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: callBack,
+      style: ElevatedButton.styleFrom(
+        elevation: 5,
+        padding: const EdgeInsets.all(16.0),
+        primary: Colors.green,
+        onPrimary: const Color.fromARGB(255, 225, 241, 233),
+      ),
+      child: Text(textButton),
+    );
+  }
+}
