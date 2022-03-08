f045d438b0bf64ff367f2235f992ba1c243675ef
Arthur Nunes <art.silvarn@gmail.com>
2022-03-07 22:11:46 -0300

    change sizedbox transaction list height

--- lib/features/home/widgets/transaction_list.dart
+++ lib/features/home/widgets/transaction_list.dart
@@ -79,7 +79,7 @@ class _TransactionListState extends State<TransactionList> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.5,
+      height: MediaQuery.of(context).size.height * 0.3,
       child: ValueListenableBuilder(
         valueListenable: _transactionController.transactionsListenable,
         builder: (_, __, ___) {
@@ -148,7 +148,6 @@ class _TransactionListState extends State<TransactionList> {
                                         context.locale().dateFormatAbrevMonth)
                                     .format(tr.date),
                               ),
-                              
                               trailing: Padding(
                                 padding: const EdgeInsets.all(6),
                                 child: Text(context.locale().valueTransaction(
