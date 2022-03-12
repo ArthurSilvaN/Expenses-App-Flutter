236d146d8699eb90c6317af5844ae72504e9b8c5
Arthur Nunes <art.silvarn@gmail.com>
2022-03-11 21:03:59 -0300

    added a user entity

---
+++ lib/entities/user.dart
@@ -0,0 +1,13 @@
+class UserFinancy {
+  UserFinancy({
+    required this.name,
+    required this.photoURL,
+    required this.id,
+    required this.email,
+  });
+
+  final String id;
+  final String name;
+  final String email;
+  final String photoURL;
+}
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,11 +4,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import '../entities/user.dart';
+
 class AuthService extends ChangeNotifier {
   final googleSignIn = GoogleSignIn();
 
   GoogleSignInAccount? _user;
   GoogleSignInAccount? get user => _user;
+
+  UserFinancy? _userFinancy;
+  UserFinancy? get userFinancy => _userFinancy;
 
   Future<void> googleLogin() async {
     try {
@@ -25,12 +30,21 @@ class AuthService extends ChangeNotifier {
         idToken: googleAuth.idToken,
       );
 
+      _userFinancy = UserFinancy(
+        id: googleUser.id,
+        name: googleUser.displayName!,
+        email: googleUser.email,
+        photoURL: googleUser.photoUrl!,
+      );
+
       await FirebaseAuth.instance.signInWithCredential(credential);
     } catch (exeption) {
       log(exeption.toString());
     }
     notifyListeners();
   }
+
+  void setUser(AsyncSnapshot<User?> userResponse) {}
 
   Future<void> logout() async {
     await googleSignIn.disconnect();
