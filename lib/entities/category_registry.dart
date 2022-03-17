1069fde880a219d3d4db5119fbe9c7c475ed01d0
Arthur Nunes <art.silvarn@gmail.com>
2022-03-16 22:30:36 -0300

    added id and userId in category

--- lib/entities/category_registry.dart
+++ lib/entities/category_registry.dart
@@ -5,21 +5,29 @@ class CategoryRegistry {
     required this.color,
     required this.name,
     required this.value,
+    required this.id,
+    required this.userId,
   });
 
   factory CategoryRegistry.fromMap(Map<String, dynamic> map) =>
       CategoryRegistry(
+        id: map['id'] as String,
+        userId: map['userId'] as String,
         name: map['name'] as String,
         value: map['valueCategory'] as double,
         color: Color(int.parse(map['colorCategory'].toString())),
       );
 
-  final Color color;
+  final String id;
   final String name;
   late double value;
+  final Color color;
+  final String userId;
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
+      'userId': userId,
       'name': name,
       'valueCategory': value,
       'colorCategory': color.value.toString(),
