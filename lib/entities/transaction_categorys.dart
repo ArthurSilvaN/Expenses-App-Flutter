8503737405db30b8611b193f9a315302ef7533a3
Arthur Nunes <art.silvarn@gmail.com>
2022-03-16 22:30:59 -0300

    added id in Category Entity

--- lib/entities/transaction_categorys.dart
+++ lib/entities/transaction_categorys.dart
@@ -4,16 +4,18 @@ import '../models/category_model.dart';
 
 class Category {
   Category({
+    required this.id,
     required this.name,
     required this.color,
     required this.icon,
   });
 
+  final String id;
   final String name;
   final Color color;
   final IconData icon;
 
   CategoryModel toModel() {
-    return CategoryModel(name: name, icon: icon, color: color);
+    return CategoryModel(name: name, icon: icon, color: color, id: id);
   }
 }
