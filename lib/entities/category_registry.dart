04ff4c10de617812802eaf5407b2cfc367597f09
Arthur Nunes <art.silvarn@gmail.com>
2022-03-17 23:49:20 -0300

    removed userId in CategoryRegistry

--- lib/entities/category_registry.dart
+++ lib/entities/category_registry.dart
@@ -6,13 +6,11 @@ class CategoryRegistry {
     required this.name,
     required this.value,
     required this.id,
-    required this.userId,
   });
 
   factory CategoryRegistry.fromMap(Map<String, dynamic> map) =>
       CategoryRegistry(
         id: map['id'] as String,
-        userId: map['userId'] as String,
         name: map['name'] as String,
         value: map['valueCategory'] as double,
         color: Color(int.parse(map['colorCategory'].toString())),
@@ -22,12 +20,10 @@ class CategoryRegistry {
   final String name;
   late double value;
   final Color color;
-  final String userId;
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'userId': userId,
       'name': name,
       'valueCategory': value,
       'colorCategory': color.value.toString(),
