6149057d44e16a0be11ee764e81910aef96871a5
Arthur Nunes <art.silvarn@gmail.com>
2022-03-19 00:31:57 -0300

    feature: floating button component scaffold home

---
+++ lib/features/home/components/floating_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/transaction_form.dart';
+
+class FloatingButtonFinancy extends StatelessWidget {
+  const FloatingButtonFinancy({Key? key, required this.homeContext})
+      : super(key: key);
+
+  final BuildContext homeContext;
+
+  @override
+  Widget build(context) {
+    return SizedBox(
+      height: 90,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () => _openTransactionFormModal(),
+            child: Container(
+              width: 56,
+              height: 56,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Icon(
+                Icons.add_box_outlined,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _openTransactionFormModal() {
+    showModalBottomSheet(
+      context: homeContext,
+      builder: (_) {
+        return SingleChildScrollView(
+          padding: EdgeInsets.only(
+              bottom: MediaQuery.of(homeContext).viewInsets.bottom),
+          child: const TransactionForm(),
+        );
+      },
+    );
+  }
+}
