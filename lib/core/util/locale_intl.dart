97bc9618ba2476c9eab8adfc4c7ece2b61544c15
Arthur Nunes <arthur.nunes@dtidigital.com.br>
2022-03-24 01:26:55 -0300

    feature: deleted files to organized imports

--- lib/core/util/locale_intl.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import '../../generated/l10n.dart';
-
-extension LocaleContextExtension on BuildContext {
-  S locale() => S.of(this);
-}
--- lib/entities/transaction_registry.dart
+++
@@ -1,6 +0,0 @@
-class TransactionRegistry {
-  TransactionRegistry(this.value, this.date);
-
-  final double value;
-  final String date;
-}
