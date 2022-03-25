c1fa2d5fc0cf9e3b3854a0f285c34b198dfbe660
Arthur Nunes <arthur.nunes@dtidigital.com.br>
2022-03-25 01:27:09 -0300

    feature: reduce transition duration and remove homeContext

--- lib/app/features/home/components/floating_button.dart
+++ lib/app/features/home/components/floating_button.dart
@@ -5,10 +5,7 @@ import '../../../core/util/mediaquery_contex.dart';
 import '../../transaction/transaction_form/transaction_form.dart';
 
 class FloatingButtonFinancy extends StatelessWidget {
-  const FloatingButtonFinancy({Key? key, required this.homeContext})
-      : super(key: key);
-
-  final BuildContext homeContext;
+  const FloatingButtonFinancy({Key? key}) : super(key: key);
 
   @override
   Widget build(context) {
@@ -17,7 +14,7 @@ class FloatingButtonFinancy extends StatelessWidget {
       width: context.sizeContext().height * 0.075,
       child: OpenContainer(
         closedElevation: 0,
-        transitionDuration: const Duration(milliseconds: 1000),
+        transitionDuration: const Duration(milliseconds: 500),
         closedShape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(5.0)),
         ),
